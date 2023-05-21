3dd47236b2d8cf630173285911dff9ce5428f693
Johan404 <cjoberholzer.p@gmail.com>
2023-05-21 21:21:59 +0200

    - Added the ability to scrape an entire site. - Obey robots.txt

--- scrapy_.py
+++ scrapy_.py
@@ -11,16 +11,24 @@ from scraper import BaseScraper
 
 from scrapy import signals
 from scrapy.exceptions import IgnoreRequest
-from scrapy.spiders import Spider, Rule
+from scrapy.spiders import Spider, Rule, CrawlSpider
 
 
-class ScrapySpider(Spider):
+class ScrapySpider(CrawlSpider):
     """
     Spider class to be used with ScrapyScraper.
     """
     name = 'doc_spider'
+    _follow_links = True
+
     allowed_domains = ['python.langchain.com']
     start_urls = []  # This will be filled dynamically
+    scrape_entire_site = False  # Determines whether to scrape the entire site. Default is False.
+
+    # This is a new addition for CrawlSpider functionality
+    rules = (
+        Rule(LinkExtractor(), callback='parse_item', follow=True),
+    )
 
     custom_settings = {
         'ITEM_PIPELINES': {'pipeline.JsonExportPipeline': 1},  # Used for Pipeline order
@@ -30,10 +38,12 @@ class ScrapySpider(Spider):
             # '__main__.ProxyMiddleware': 100
             'middleware.ErrorLoggingMiddleware': 543,
         },
+        'ROBOTSTXT_OBEY': True,  # Added this setting to obey robots.txt
     }
 
-    def __init__(self, crawler, urls=None, *args, **kwargs):
+    def __init__(self, crawler, urls=None, scrape_entire_site=False, *args, **kwargs):
         self.crawler = crawler
+        self.scrape_entire_site = scrape_entire_site
         super().__init__(*args, **kwargs)
         if urls is not None:
             self.start_urls = urls
@@ -42,6 +52,12 @@ class ScrapySpider(Spider):
         self.crawler.signals.connect(self.spider_closed, signals.spider_closed)
 
         self.crawler.signals.connect(self.handle_spider_error, signals.spider_error)
+
+    def parse_item(self, response):
+        item = DocItem()
+        item['api_docs'] = response.css('pre::text').getall()  # extracting API documentation
+        item['explanatory_text'] = response.css('p::text').getall()  # extracting explanatory text
+        yield item
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
@@ -86,14 +102,14 @@ class ScrapyScraper(BaseScraper, scrapy.Spider):
         :return: a pandas DataFrame with the scraped data.
         """
         process = CrawlerProcess()
-        process.crawl(ScrapySpider, urls=urls)
+        process.crawl(crawler_or_spidercls=ScrapySpider, urls=urls, scrape_entire_site=True)
         process.start()  # the script will block here until the crawling is finished
 
         # Wrap the file reading code with try/except
         try:
             # TODO: Read the output JSON file into a pandas DataFrame
             df = pd.read_json("output/items.json")
-            df.to_csv('output.csv', index=False)
+            df.to_csv('output/output.csv', index=False)
             # return df
         except FileNotFoundError:
             self.logger.error('Failed to read JSON file. File not found.', exc_info=True)
@@ -105,5 +121,8 @@ class ScrapyScraper(BaseScraper, scrapy.Spider):
 Running the scraper
 """
 LOG_LEVEL = 'DEBUG'
+ROBOTSTXT_OBEY = True
+
+
 scrapy_scraper = ScrapyScraper()
 scrapy_scraper.scrape_site(["https://python.langchain.com/en/latest/index.html"])
